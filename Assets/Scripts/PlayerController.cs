8464eaaebde5ce90e71a0c211fa3edfe28718638
Jonathan <d016527j@student.staffs.ac.uk>
2022-01-14 10:47:36 +0000

    Fixes Character Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,21 @@ public class PlayerController : MonoBehaviour
 {
 	[Header("Input Settings")]
 	[SerializeField]
-	bool mouseInput = false;
+	private bool mouseInput = false;
 	[SerializeField]
-	float moveSpeed = 3.0f;
+	private float moveSpeed = 3.0f;
 	[SerializeField]
-	Vector2 inputDeadZone = new Vector2(0.2f, 0.2f);
+	private Vector2 inputDeadZone = new Vector2(0.2f, 0.2f);
 	[SerializeField]
-	float slowSpeed = 0.9f;
-
-	[Header("Camera Settings")]
+	private float maxSpeed = 2.0f;
+	[SerializeField]
+	private float slowSpeed = 0.9f;
+	
+	[Header("Camera Settings")] 
 	[SerializeField]
 	private Camera cam;
+	[SerializeField]
+	private bool smoothCam = true;
 	[SerializeField]
 	private float camSmoothSpeed = 1.0f;
 	[SerializeField]
@@ -27,6 +31,8 @@ public class PlayerController : MonoBehaviour
 	private Vector3 angleOffset;
 	
 	private Rigidbody rb;
+	private bool xInputPressed = false;
+	private bool yInputPressed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -37,41 +43,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+		ConstrainVelocity();
 		UpdatePlayerInput();
 		UpdateCameraPos();
 	}
 
+	private void ConstrainVelocity()
+    {
+		if(rb.velocity.magnitude > maxSpeed)
+        {
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+
 	private void UpdatePlayerInput() {
-		Debug.Log("forward: " + cam.transform.forward.normalized);
-		Debug.Log("right: " + cam.transform.right.normalized);
-		Debug.Log("up: " + cam.transform.up.normalized);
 		float xInput = Input.GetAxis("Horizontal");
 		float yInput = Input.GetAxis("Vertical");
 		
-		Vector3 camDirX = new Vector3(cam.transform.right.normalized.x, 0, cam.transform.right.normalized.z);
-		Vector3 camDirY = new Vector3(cam.transform.right.normalized.x, 0, cam.transform.right.normalized.z);
+		Vector3 camDirX = new Vector3(Mathf.Round(cam.transform.right.normalized.x), 0, Mathf.Round(cam.transform.right.normalized.z));
+		Vector3 camDirY = new Vector3(Mathf.Round(cam.transform.forward.normalized.x), 0, Mathf.Round(cam.transform.forward.normalized.z));
 
 		if (xInput > inputDeadZone.x || xInput < -inputDeadZone.x) {
 			rb.AddForce(camDirX * (xInput * moveSpeed));
-
+			xInputPressed = true;
 		} else {
-			if (rb.velocity.x > 0.05 || rb.velocity.x < 0.05) {
-				rb.velocity = rb.velocity * slowSpeed;
+			xInputPressed = false;
+		}
+
+		if (yInput > inputDeadZone.y || yInput < -inputDeadZone.y) {
+			rb.AddForce(camDirY * (yInput * moveSpeed));
+			yInputPressed = true;
+        } else {
+			yInputPressed = false;
+		}
+
+		if (!xInputPressed && !yInputPressed) {
+			if (rb.velocity.magnitude > 0.05 || rb.velocity.magnitude < 0.05) {
+				rb.velocity = rb.velocity * slowSpeed; 
 			} else {
 				rb.velocity = Vector3.zero;
 			}
 		}
 
-		if (yInput > inputDeadZone.y || yInput < -inputDeadZone.y) {
-			rb.AddForce(cam.transform.forward.normalized * (yInput * moveSpeed));
-
-		} // else {
-			//if (rb.velocity.z > 0.05 || rb.velocity.z < 0.05) {
-			//	rb.velocity = cam.transform.forward.normalized * slowSpeed;
-			//} else {
-			//	rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
-			//}
-		// }
 	}
 
 	private void UpdateCameraPos() {
@@ -79,8 +92,13 @@ public class PlayerController : MonoBehaviour
 									transform.position.y + heightOffset,
 									transform.position.z + distanceOffset);
 
-		cam.transform.position = Vector3.Lerp(cam.transform.position, endPos, camSmoothSpeed * Time.deltaTime);
+		cam.transform.position = smoothCam ?
+			Vector3.Lerp(cam.transform.position, endPos, camSmoothSpeed * Time.deltaTime) :
+			endPos;
+		
+
 		cam.transform.rotation = Quaternion.Euler(angleOffset.x, angleOffset.y, angleOffset.z);
+
 	}
 
 }
