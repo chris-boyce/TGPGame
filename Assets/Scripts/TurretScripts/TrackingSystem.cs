f53ada4e0ace09226cbabc25e2f057f4f428435b
Lucy Power <74964639+lucypower@users.noreply.github.com>
2022-01-14 15:50:12 +0000

    Tracking System

---
+++ Assets/Scripts/TurretScripts/TrackingSystem.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingSystem : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public GameObject target = null;
+    Vector3 lastPosition = Vector3.zero;
+    Quaternion lookRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target)
+        {
+            if(lastPosition != target.transform.position)
+            {
+                lastPosition = target.transform.position;
+                lookRotation = Quaternion.LookRotation(lastPosition - transform.position);
+            }
+
+            if(transform.rotation != lookRotation)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    bool SetTarget(GameObject _target)
+    {
+        if(!target)
+        {
+            return false;
+        }
+
+        target = _target;
+
+        return true;
+    }
+}
