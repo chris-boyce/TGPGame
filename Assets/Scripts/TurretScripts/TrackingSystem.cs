1b9046bb3c06fbd6e7e2ed4c6ff03a16f59c480d
lucypower <luciellepenguin9@gmail.com>
2022-01-17 16:34:31 +0000

    monday

--- Assets/Scripts/TurretScripts/TrackingSystem.cs
+++ Assets/Scripts/TurretScripts/TrackingSystem.cs
@@ -9,9 +9,25 @@ public class TrackingSystem : MonoBehaviour
     Vector3 lastPosition = Vector3.zero;
     Quaternion lookRotation;
 
+    public GameObject bullet;
+    public GameObject gun;
+    public Transform bulletPosition;
+    public ParticleSystem muzzle;
+    public float fireRate = 15.0f;
+    private float nextFire = 0.0f;
+
+
+    void Start()
+    {
+        gun.SetActive(true);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        Vector3 a = (target.transform.position - transform.position).normalized;
+        float dot = Vector3.Dot(a, transform.forward);
+
         if(target)
         {
             if(lastPosition != target.transform.position)
@@ -23,6 +39,16 @@ public class TrackingSystem : MonoBehaviour
             if(transform.rotation != lookRotation)
             {
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, speed * Time.deltaTime);
+            }
+
+            if(dot != 0)
+            {
+                Shoot();
+                //return;
+            }
+            else
+            {
+                Shoot();
             }
         }
     }
@@ -37,5 +63,12 @@ public class TrackingSystem : MonoBehaviour
         target = _target;
 
         return true;
+    }
+
+    void Shoot()
+    {
+        muzzle.Play();
+        nextFire = Time.time + 1f / fireRate;
+        Instantiate(bullet, bulletPosition.transform.position, bulletPosition.transform.rotation);
     }
 }
