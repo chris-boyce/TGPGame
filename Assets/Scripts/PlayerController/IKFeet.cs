0413e8c49d7b2143361aef8dd2e3bc44009688aa
Jonathan <d016527j@student.staffs.ac.uk>
2022-01-25 15:54:25 +0000

    zombie prefab added

--- Assets/Scripts/PlayerController/IKFeet.cs
+++ Assets/Scripts/PlayerController/IKFeet.cs
@@ -67,6 +67,10 @@ public class IKFeet : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate() {
+		if (!root) {
+			Destroy(this);
+			return;
+		}
 		if(calcVelocity)	velocity = (root.position - oldRootPos);
 
 		float magnitude;
