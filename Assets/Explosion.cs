1e9b074b08fcb38a23d116dcdcac756bfc47f949
Bstafs <Bstafs@gmail.com>
2022-01-28 13:44:13 +0000

    Turrent Sound

--- Assets/Explosion.cs
+++ Assets/Explosion.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Explosion : MonoBehaviour
 {
-
+    public AudioSource explosionSound;
     public ParticleSystem explosion;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Floor"))
         {
             explosion.Play();
+            explosionSound.Play();
         }
     }
 
