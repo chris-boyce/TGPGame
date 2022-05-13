773ec2f2b9d4794b1671598d11c3a3d1e354af93
Christopher Boyce <57403492+chris-boyce@users.noreply.github.com>
2022-05-13 09:45:02 +0100

    Commment Changed

--- Assets/Scripts/UISCRIPTS/WeaponHud.cs
+++ Assets/Scripts/UISCRIPTS/WeaponHud.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class WeaponHud : MonoBehaviour
 {
-    //public WeaponSwitcher _WeaponSwitchReference;
     public WeaponSelector WeaponSelector;
     public GameObject _PistolImage;
     public GameObject _SniperImage;
@@ -20,97 +19,6 @@ public class WeaponHud : MonoBehaviour
 
     void Update()
     {
-
-        //DONT OPEN ITS UGLY
-
-        //FOR EACH WEAPON SELECTION - EACH CASE WILL DISABLE ALL OTHER WEAPONS AS SELECTED CHOICE , MAKE THE GUN BOUND TO INT (KEY BOARD INPUT) SELECT IMAGE ACTIVE
-        /*
-        switch(_WeaponSwitchReference.selectedWeapon)
-        {
-
-            case 0:
-                _PistolImage.SetActive(true);
-                
-                _SniperImage.SetActive(false);
-                _MachinePistol.SetActive(false);
-                _P90Image.SetActive(false);
-                _AKImage.SetActive(false);
-                _M16Image.SetActive(false);
-                _ShotgunImage.SetActive(false);
-
-                break;
-            case 1:
-
-                _SniperImage.SetActive(true);
-               
-                _PistolImage.SetActive(false);
-                _MachinePistol.SetActive(false);
-                _P90Image.SetActive(false);
-                _AKImage.SetActive(false);
-                _M16Image.SetActive(false);
-                _ShotgunImage.SetActive(false);
-
-                break;
-            case 2:
-
-               _MachinePistol.SetActive(true);
-                
-                _PistolImage.SetActive(false);
-                _SniperImage.SetActive(false);
-                _P90Image.SetActive(false);
-                _AKImage.SetActive(false);
-                _M16Image.SetActive(false);
-                _ShotgunImage.SetActive(false);
-                break;
-            case 3:
-
-                _P90Image.SetActive(true);
-                
-                _MachinePistol.SetActive(false);
-                _PistolImage.SetActive(false);
-                _SniperImage.SetActive(false);
-                _AKImage.SetActive(false);
-                _M16Image.SetActive(false);
-                _ShotgunImage.SetActive(false);
-                break;
-            case 4:
-
-                _AKImage.SetActive(true);
-               
-                _P90Image.SetActive(false);
-                _MachinePistol.SetActive(false);
-                _PistolImage.SetActive(false);
-                _SniperImage.SetActive(false);
-                _M16Image.SetActive(false);
-                _ShotgunImage.SetActive(false);
-                break;
-            case 5:
-                _M16Image.SetActive(true);
-
-                _AKImage.SetActive(false);
-                _P90Image.SetActive(false);
-                _MachinePistol.SetActive(false);
-                _PistolImage.SetActive(false);
-                _SniperImage.SetActive(false);
-                _ShotgunImage.SetActive(false);
-                break;
-            case 6:
-                _ShotgunImage.SetActive(true);
-
-                
-                _M16Image.SetActive(false);
-                _PistolImage.SetActive(false);
-                _SniperImage.SetActive(false);
-                _AKImage.SetActive(false);
-                _P90Image.SetActive(false);
-                _MachinePistol.SetActive(false);
-
-                break;
-
-
-
-        }
-        */
         switch(WeaponSelector.CurrentWeapon)
         {
             case PickupType.AK:
