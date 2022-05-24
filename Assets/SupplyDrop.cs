113fb33a42153d3257e6301b680be82391e9f413
Christopher Boyce <57403492+chris-boyce@users.noreply.github.com>
2022-05-24 13:52:19 +0100

    Beep Boop

---
+++ Assets/SupplyDrop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupplyDrop : MonoBehaviour
+{
+    public GameObject SupplyDropUI;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SupplyDropUI.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SupplyDropUI.SetActive(false);
+        }
+    }
+
+}
