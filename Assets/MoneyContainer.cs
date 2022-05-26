a1ec249d0bcb11ed64dc9f7bdc895a27ecb0421e
Christopher Boyce <57403492+chris-boyce@users.noreply.github.com>
2022-05-26 21:59:49 +0100

    Coin Update

---
+++ Assets/MoneyContainer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyContainer : MonoBehaviour
+{
+    private CoinPickup CP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CP = GameObject.FindGameObjectWithTag("Player").GetComponent<CoinPickup>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("KILL ME");
+            CP.AmountOfMoney = CP.AmountOfMoney + 50;
+            Destroy(this.gameObject);
+        }
+    }
+
+}
